1d7e8985a793d81c84b479a94bdef6a7db9e01a1
turnbew <turnbew.co.uk@gmail.com>
2018-04-03 12:51:51 +0100

    Add files via upload

---
+++ network-site/addons/default/modules/network_settings/language/english/network_settings_lang.php
@@ -0,0 +1,58 @@
+<?php
+//messages
+$lang['network_settings:success']			=	'It worked';
+$lang['network_settings:error']			=	'It didn\'t work';
+$lang['network_settings:no_items']		=	'No Items';
+
+//page titles
+$lang['network_settings:create']			=	'Create Item';
+
+//labels
+$lang['network_settings:name']			=	'Name';
+$lang['network_settings:slug']			=	'Slug';
+$lang['network_settings:manage']			=	'Manage';
+$lang['network_settings:item_list']		=	'Item List';
+$lang['network_settings:view']			=	'View';
+$lang['network_settings:edit']			=	'Edit';
+$lang['network_settings:delete']			=	'Delete';
+
+//buttons
+$lang['network_settings:custom_button']	=	'Custom Button';
+$lang['network_settings:items']			=	'Items';
+
+//Activity Tracker
+// Delete
+$lang['activity_tracker_mass_delete_success']              = '%s activities out of %s successfully deleted.';
+$lang['user_list_mass_delete_success']                     = '%s users out of %s successfully removed from activity';
+$lang['activity_tracker_mark_done_success']                = '%s activities of %s successfully marked as done';
+$lang['activity_tracker_email_was_sent_name']              = 'Sent: %s<br>Subject: %s<br>From: %s<br>To: %s';
+
+//General Setups
+$lang['default_currency'] = 'Default currency';
+$lang['default_timezone'] = 'Default timezone';
+$lang['newsletters:send_on_timezone:label'] = 'Time zones';
+$lang['newsletters:instructions:send_on_timezone'] = 'Scheduled e-mail will be sent in determined time zone.';
+
+//network_settings/view/content/announcements
+$lang['display:latest:archives'] = 'Display latest archives box at top of the page';
+
+//General Setups
+$lang['users:create:already_exists'] = 'A user with these details already exists in the database. Click CREATE to proceed.';
+
+
+//Galleries
+//Publish
+$lang['gallery_publish_success']                     = 'The gallery has now been published to the Galleries Page.';
+$lang['gallery_unpublish_success']                   = 'The gallery has now been removed from the Galleries Page.';
+
+//Admin-tab
+$lang['network_settings:admin_title']			=	'Offline record updates (admin only)';
+$lang['network_settings:online_title']			=	'Online profile updates';
+//Upgrade Modules
+$lang['modules:upgrade_success_all']     = 'Modules "%s" upgraded successfully.';
+$lang['modules:upgrade_error']           = 'Could not upgrade module "%s".';
+
+//System Fields
+$lang['system_fields:label:custom_fields'] = 'Custom Fields';
+
+?>
---
+++ network-site/addons/default/modules/network_settings/views/email_center/templates/edit_frequency_field.php
@@ -0,0 +1,11 @@
+											<li>
+												<label for="frequency">
+													<?=lang('system_emails:label:frequency')?>?
+													<a class="my-tool-tip" data-toggle="tooltip" data-placement="right" title="" data-original-title="<?=lang('system_emails:info:frequency')?>"> 
+														<span class="glyphicon glyphicon-question-sign" style="color: #999;"></span>
+													</a>
+												</label>
+												<div class="input">
+												<?=$frequencySelectList?>
+												</div>
+											</li>
